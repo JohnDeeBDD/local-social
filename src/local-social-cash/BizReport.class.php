14a899ea97e1b82f147bc3cb19f8f6754f5bbba4
John Dee <johndeebdd@gmail.com>
2018-09-30 22:28:11 -0700

    no id3

--- src/local-social-cash/BizReport.class.php
+++ src/local-social-cash/BizReport.class.php
@@ -21,13 +21,10 @@ th {
 table, th, td {
    border: 1px solid black;
 }
-th, td {
-    padding: 15px;
-    text-align: left;
-}
+
 </style>
 <table>";
-        $output = $output . "<tr><td>ID</td><td>First Name</td><td>Last Name</td><td>Phone</td><td>Email</td><td>Facebook</td></tr>";
+        $output = $output . "<tr><td>First Name</td><td>Last Name</td><td>Phone</td><td>Email</td><td>Facebook</td></tr>";
         foreach ( $blogusers as $user ) {
             $ID = $user->ID;
             $output = $output . "<tr>";
