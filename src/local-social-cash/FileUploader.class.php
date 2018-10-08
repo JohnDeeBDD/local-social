ad884f4f8e3fd6d5cf6467c5658741b0d1d92985
John Dee <johndeebdd@gmail.com>
2018-10-08 15:45:13 -0700

    fed

--- src/local-social-cash/FileUploader.class.php
+++ src/local-social-cash/FileUploader.class.php
@@ -2,32 +2,38 @@
 namespace LocalSocialCash; 
 
 class FileUploader{    
-        pubic function returnReportHTML(){
-            $output = <<<output
- <form>
- </form>
- output;
-            return $output;
-        }
+    
+    public function returnReportHTML(){
+        /*
+        firstName
+        lastName
+        email
+        phone
+        zip
+        FacebookUrl
+        file
+        */
+        $output = <<<output
+<form>
+   This is a form.
+</form>
+output;
+        return $output;
+    }
         
-        public function xxx(){
+    public function xxx(){
         if(is_user_logged_in()){ 
             $output = "";
-          $user =  wp_get_current_user();
-          global $post; 
+            $user =  wp_get_current_user();
+            global $post; 
             $output = $output ."User ID : ". $user->ID; 
             $output = $output ."<br> "; 
-            $output = $output ."Post ID : ". $post->ID;  
-
-
-        return $output;
-    
-}
-    else 
-        $url = get_home_url();
-        wp_redirect( $url );
-exit;
-    }
-    
- 
+            $output = $output ."Post ID : ". $post->ID;
+            return $output;
+        }else{
+            $url = get_home_url();
+            wp_redirect( $url );
+            exit;
+        }
+    }   
 }
