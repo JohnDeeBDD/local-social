d536cffde916083c4413dcbbbcc21814c406d502
surender <surendersharma48@gmail.com>
2018-10-09 16:09:36 +0530

    file saved work done

--- src/local-social-cash/FileUploader.class.php
+++ src/local-social-cash/FileUploader.class.php
@@ -12,23 +12,69 @@ class FileUploader{
         FacebookUrl
         file
         */
+
         $output = <<<output
+
 <form method='POST' enctype='multipart/form-data'>
   <input type="file" name ="userFile" />
   <input type="submit" name="file-from-form"/>
 </form>
 
 output;
+
         return $output;
     }
 
-    public function listenToFormSubmission(){
 
- 
+public function listenToFormSubmission(){
 
+$target_dir = wp_upload_dir();
+$target_file = $target_dir[path]."/".basename($_FILES['userFile']['name']);
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+// Check if image file is a actual image or fake image
 
+    $check = getimagesize($_FILES["userFile"]["tmp_name"]);
+    if($check !== false) {
+        //echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        //echo "File is not an image.";
+        $uploadOk = 0;
+    }
+
+// Check if file already exists
+if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
+}
+// Check file size
+if ($_FILES["userFile"]["size"] > 500000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+}
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" ) {
+    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+    $uploadOk = 0;
+}
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+    //print_r($target_file);
+   $temp = explode(".", $_FILES["userFile"]["name"]);
+   $newfilename = round(microtime(true)) . '.' . end($temp);
+    if (move_uploaded_file($_FILES["userFile"]["tmp_name"], $target_file. $newfilename)) {
+    echo "The file ". basename( $_FILES["userFile"]["name"]). " has been uploaded.";
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
+}
     }
         
+
     public function xxx(){
         if(is_user_logged_in()){ 
             $output = "";
